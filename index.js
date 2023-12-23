60ddb69aa6493f47aeec6424545766e54d9ac063
Evan <edjones319@gmail.com>
2023-12-23 16:12:59 -0500

    complete

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+function writeCards(names) {
+
+    const resultArray = [] 
+
+    for (let name of names) {
+      resultArray.push(`Thank you, ${name}, for the wonderful surprise gift!`)
+    }
+
+    return resultArray
+}
+
+function countDown (count) {
+    while (count >= 0) {
+        console.log(count);
+        count--;
+    }
+}
